2c31a4417acfbf94adf6054348969b4cb2254227
Arron <beale86@msn.com>
2023-07-04 19:41:36 +0000

    Refactor for different states

--- src/App.js
+++ src/App.js
@@ -37,6 +37,19 @@ function App() {
     setIsLoading(false);
   };
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if(isLoading) {
+    content = <p>Loading...</p>;
+  }
 
 
   return (
@@ -44,12 +57,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
